b175e4f75a1eaeabb4aa8f5640a2f774b5117878
bbilog <brian.bilog@searchoptmedia.com>
2016-06-03 20:20:24 +0800

    no message

--- web/webhook.php
+++ web/webhook.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../vendor/autoload.php';
+//require_once '../vendor/autoload.php';
 // use FacebookAds\Api;
 // use FacebookAds\Object\Lead;
 // Api::init('287026681636328','d11f7cab83ce6d228703dd3defd4f05e','EAAEFDJtmZAegBAKvDJpELkpZCQQFbRYuq5ArCdPFBqs9536392mV7WkF1MkFkjjpdzCAplZAHSoXyswarQoIbRgPDZA5cQ4T59KlrtpLkhZAPeY3hd5sTEnTAA4LhwEa7w5jQlSvQt4jB96PyqdMCdnteOu9oi2MZD');
