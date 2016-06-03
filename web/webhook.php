6b351701b55b8cad6dde469a4dc4199e44df891a
bbilog <brian.bilog@searchoptmedia.com>
2016-06-03 20:53:04 +0800

    Update webhook.php

--- web/webhook.php
+++ web/webhook.php
@@ -1,8 +1,12 @@
 <?php
+$challenge = $_REQUEST['hub_challenge'];
+$verify_token = $_REQUEST['hub_verify_token'];
 
-require_once '../vendor/autoload.php';
-use FacebookAds\Api;
-use FacebookAds\Object\Lead;
-Api::init('287026681636328','d11f7cab83ce6d228703dd3defd4f05e','EAAEFDJtmZAegBAKvDJpELkpZCQQFbRYuq5ArCdPFBqs9536392mV7WkF1MkFkjjpdzCAplZAHSoXyswarQoIbRgPDZA5cQ4T59KlrtpLkhZAPeY3hd5sTEnTAA4LhwEa7w5jQlSvQt4jB96PyqdMCdnteOu9oi2MZD');
+if ($verify_token === 'abc123') {
+  echo $challenge;
+}
+
+$input = json_decode(file_get_contents('php://input'),true);
 
 
+error_log(print_r($input, true));
