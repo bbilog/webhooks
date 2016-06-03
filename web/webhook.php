8ed87c7f2f9133fbb67942bd0f97a2be5c00badf
bbilog <brian.bilog@searchoptmedia.com>
2016-06-03 18:29:26 +0800

    Update webhook.php

--- web/webhook.php
+++ web/webhook.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../vendor/autoload.php';
+require_once __DIR__ . '../vendor/facebook/php-sdk-v4/src/Facebook/autoload.php';
 use FacebookAds\Object\Lead;
 
 $challenge = $_REQUEST['hub_challenge'];
