fad3e21edf8c665bb3b45761c2803f8b64811f50
bbilog <brian.bilog@searchoptmedia.com>
2016-06-03 15:37:58 +0800

    Update webhook.php

--- web/webhook.php
+++ web/webhook.php
@@ -1,4 +1,8 @@
 <?php
+
+// require_once 'vendor/autoload.php';
+// use FacebookAds\Object\Lead;
+
 $challenge = $_REQUEST['hub_challenge'];
 $verify_token = $_REQUEST['hub_verify_token'];
 
@@ -7,4 +11,16 @@ if ($verify_token === 'abc123') {
 }
 
 $input = json_decode(file_get_contents('php://input'),true);
-error_log(print_r($input, true));
+
+
+$form_id = $input['entry'][0]['changes'][0]['value']['form_id']; 
+$leadgen_id = $input['entry'][0]['changes'][0]['value']['leadgen_id']; 
+$page_id = $input['entry'][0]['changes'][0]['value']['leadgen_id']; 
+$created_time = $input['entry'][0]['changes'][0]['value']['created_time']; 
+
+// $form = new Lead($form_id);
+// $leads = $form->read();
+
+error_log(print_r($form_id, true));
+
+
