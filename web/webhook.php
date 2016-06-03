c009f74ed65325832f482ce6df02976846db2212
bbilog <brian.bilog@searchoptmedia.com>
2016-06-03 20:09:44 +0800

    no message

--- web/webhook.php
+++ web/webhook.php
@@ -6,3 +6,14 @@ use FacebookAds\Object\Lead;
 Api::init('287026681636328','d11f7cab83ce6d228703dd3defd4f05e','EAAEFDJtmZAegBAKvDJpELkpZCQQFbRYuq5ArCdPFBqs9536392mV7WkF1MkFkjjpdzCAplZAHSoXyswarQoIbRgPDZA5cQ4T59KlrtpLkhZAPeY3hd5sTEnTAA4LhwEa7w5jQlSvQt4jB96PyqdMCdnteOu9oi2MZD');
 
 
+$challenge = $_REQUEST['hub_challenge'];
+$verify_token = $_REQUEST['hub_verify_token'];
+
+if ($verify_token === 'abc123') {
+  echo $challenge;
+}
+
+$input = json_decode(file_get_contents('php://input'),true);
+
+
+error_log(print_r($input, true));
