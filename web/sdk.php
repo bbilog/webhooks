c3797b84051377154efee30772c5ac7df0d09e7b
bbilog <brian.bilog@searchoptmedia.com>
2016-06-07 13:45:27 +0800

    Update sdk.php

--- web/sdk.php
+++ web/sdk.php
@@ -24,6 +24,6 @@ Api::init(
   '287026681636328','d11f7cab83ce6d228703dd3defd4f05e','EAAEFDJtmZAegBAHdYOpWwZC706O3vBW3EwPfz0yicS3dzvZCeWRTe1izRFjNsOCW917KpY2zPotU96feePfFj2ZCNW4CRz98FazvkQIRZAFDS9tZA0Mcd5JFhJXe8QJM020jpPsL4FUq9RiKlutQA8vaAoafPSa7oZD'
 );
 
-$form = new Lead($lead_id);
-$test = $form->read();
-error_log(print_r($lead_id, true));
+$leads = new Lead($lead_id);
+$leads->read();
+error_log(print_r($leads->field_data, true));
