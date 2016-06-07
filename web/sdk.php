239c9bfa1277853ec9879592cd8f8c8a2f8156c1
bbilog <brian.bilog@searchoptmedia.com>
2016-06-07 12:03:09 +0800

    Update sdk.php

--- web/sdk.php
+++ web/sdk.php
@@ -13,16 +13,17 @@ $form_id = $input['entry'][0]['changes'][0]['value']['form_id'];
 $lead_id = $input['entry'][0]['changes'][0]['value']['leadgen_id'];
 $page_id = $input['entry'][0]['changes'][0]['value']['page_id'];
 
-$test = array('form_id' => $form_id,'lead_id' => $lead_id,'page_id' => $page_id );
+//$test = array('form_id' => $form_id,'lead_id' => $lead_id,'page_id' => $page_id );
+
+require_once  '../vendor/autoload.php';
+
+use FacebookAds\Api;
+use FacebookAds\Object\Lead;
+
+Api::init(
+  '287026681636328','d11f7cab83ce6d228703dd3defd4f05e','EAAEFDJtmZAegBAHdYOpWwZC706O3vBW3EwPfz0yicS3dzvZCeWRTe1izRFjNsOCW917KpY2zPotU96feePfFj2ZCNW4CRz98FazvkQIRZAFDS9tZA0Mcd5JFhJXe8QJM020jpPsL4FUq9RiKlutQA8vaAoafPSa7oZD'
+);
+
+$form = new Lead($form_id);
+$test = $form->read();
 error_log(print_r($test, true));
-// require_once  '../vendor/autoload.php';
-
-// use FacebookAds\Api;
-// use FacebookAds\Object\Lead;
-
-// Api::init(
-//   '287026681636328','d11f7cab83ce6d228703dd3defd4f05e','EAAEFDJtmZAegBAHdYOpWwZC706O3vBW3EwPfz0yicS3dzvZCeWRTe1izRFjNsOCW917KpY2zPotU96feePfFj2ZCNW4CRz98FazvkQIRZAFDS9tZA0Mcd5JFhJXe8QJM020jpPsL4FUq9RiKlutQA8vaAoafPSa7oZD'
-// );
-
-/*$form = new Lead(<LEAD_ID>);
-$form->read();*/
