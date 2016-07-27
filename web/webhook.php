1f090b1bcd5b2f97b31a4f37b8eb6356161ec9e8
Brian Bilog <brian.bilog@searchoptmedia.com>
2016-07-27 13:02:56 +0800

    no message

--- web/webhook.php
+++ web/webhook.php
@@ -1,12 +1,13 @@
 <?php
-require_once  __DIR__.'/../vendor/autoload.php';
+require_once  __DIR__.'../vendor/autoload.php';
 
 use FacebookAds\Api;
 use FacebookAds\Object\Lead;
 use FacebookAds\Object\Fields\LeadFields;
 
-echo 'asdf';
-
 $leads = new Lead('1019631454816193');
 $asf = $leads->read();
 var_dump($asf);
+
+echo 'asdf';
+
