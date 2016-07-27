40abb8ef2aa83a96d6d140b9b8fb94c9b834083f
Brian Bilog <brian.bilog@searchoptmedia.com>
2016-07-27 12:34:36 +0800

    sdk update for leads info

--- web/sdk.php
+++ web/sdk.php
@@ -1,10 +1,10 @@
 <?php
 
-//require_once  __DIR__.'/../vendor/autoload.php';
+require_once  __DIR__.'/../vendor/autoload.php';
 
-// use FacebookAds\Api;
-// use FacebookAds\Object\Lead;
-// use FacebookAds\Object\Fields\LeadFields;
+use FacebookAds\Api;
+use FacebookAds\Object\Lead;
+use FacebookAds\Object\Fields\LeadFields;
 
 $challenge = $_REQUEST['hub_challenge'];
 $verify_token = $_REQUEST['hub_verify_token'];
@@ -14,13 +14,13 @@ if ($verify_token === 'propelrr123abc321') {
 }
 
 $input = json_decode(file_get_contents('php://input'),true);
-error_log(print_r($input, true));
-// $form_id = $input['entry'][0]['changes'][0]['value']['form_id'];
-// $lead_id = $input['entry'][0]['changes'][0]['value']['leadgen_id'];
-// $page_id = $input['entry'][0]['changes'][0]['value']['page_id'];
+//error_log(print_r($input, true));
+$form_id = $input['entry'][0]['changes'][0]['value']['form_id'];
+$lead_id = $input['entry'][0]['changes'][0]['value']['leadgen_id'];
+$page_id = $input['entry'][0]['changes'][0]['value']['page_id'];
 
-// //$test = array('form_id' => $form_id,'lead_id' => $lead_id,'page_id' => $page_id );
+//$test = array('form_id' => $form_id,'lead_id' => $lead_id,'page_id' => $page_id );
 
-// $leads = new Lead($lead_id);
-// $leads->read();
-// error_log(print_r($leads->field_data, true));
+$leads = new Lead($lead_id);
+$leads->read();
+error_log(print_r($leads->field_data, true));
