e5fd21dcff2a69924316e31a7800df15ef8a96e8
Brian Bilog <brian.bilog@searchoptmedia.com>
2016-07-27 12:57:33 +0800

    asdfdsf

--- web/webhook.php
+++ web/webhook.php
@@ -1,25 +1,8 @@
 <?php
-$challenge = $_REQUEST['hub_challenge'];
-$verify_token = $_REQUEST['hub_verify_token'];
+require_once  __DIR__.'/../vendor/autoload.php';
 
-if ($verify_token === 'abc123') {
-  echo $challenge;
-}
+use FacebookAds\Api;
+use FacebookAds\Object\Lead;
+use FacebookAds\Object\Fields\LeadFields;
 
-$input = json_decode(file_get_contents('php://input'),true);
-
-<<<<<<< HEAD
-$challenge = $_REQUEST['hub_challenge'];
-$verify_token = $_REQUEST['hub_verify_token'];
-
-if ($verify_token === 'abc123') {
-  echo $challenge;
-}
-
-$input = json_decode(file_get_contents('php://input'),true);
-
-error_log(print_r($input, true));
-=======
-
-error_log(print_r($input, true));
->>>>>>> c3797b84051377154efee30772c5ac7df0d09e7b
+echo 'asdf';
