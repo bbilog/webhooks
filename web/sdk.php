5f2833ee2eeb97375ffab95867971611f3a101f9
Brian Bilog <brian.bilog@searchoptmedia.com>
2016-07-27 11:56:09 +0800

    asdfasdf

--- web/sdk.php
+++ web/sdk.php
@@ -9,7 +9,7 @@ use FacebookAds\Object\Fields\LeadFields;
 $challenge = $_REQUEST['hub_challenge'];
 $verify_token = $_REQUEST['hub_verify_token'];
 
-if ($verify_token === 'propelrr123@@!!') {
+if ($verify_token === 'propelrr123abc321') {
   echo $challenge;
 }
 
