2677b50895b7e33daa6eddc7b29769d3261b4dbb
Brian Bilog <brian.bilog@searchoptmedia.com>
2016-07-28 18:55:12 +0800

    update form

--- web/settings.php
+++ web/settings.php
@@ -51,7 +51,7 @@ if(isset($_POST['submit']) && !empty($_POST['submit']) && isset($_POST['action']
 			<h4>Greeting Text</h4>
 			<p>Set a greeting for new conversations. It is only rendered the first time the user interacts with a the Page on Messenger.</p>
 
-			<form action="." method="post">
+			<form action="" method="post">
 				<label for="greeting">Greetings</label><br><textarea name="greeting" id="greeting"></textarea><br>
 				<input type="hidden" name="action" value="add_greeting" /><input type="submit" value="Submit" name="submit"/>
 			</form>
@@ -59,7 +59,7 @@ if(isset($_POST['submit']) && !empty($_POST['submit']) && isset($_POST['action']
 			<h4>Persistent Menu</h4>
 			<p>The Persistent Menu is a menu that is always available to the user.The menu can be invoked by a user, by tapping on the 3-caret icon on the left of the composer.</p>
 
-			<form action="." method="post">
+			<form action="" method="post">
 				<table>
 					<tr><td><label for="menu1">Menu 1</label><input type="text" value="" name="menu1" id="menu1" /></td><td><label for="pmenu1">Menu 1 payload</label><input type="text" value="" name="pmenu1" id="pmenu1" /></td></tr>
 					<tr><td><label for="menu2">Menu 1</label><input type="text" value="" name="menu2" id="menu2" /></td><td><label for="pmenu1">Menu 1 payload</label><input type="text" value="" name="pmenu2" id="pmenu2" /></td></tr>
@@ -67,7 +67,7 @@ if(isset($_POST['submit']) && !empty($_POST['submit']) && isset($_POST['action']
 				</table><br>
 				<input type="hidden" name="action" value="pmenu_add" /><input type="submit" value="Add Persistent Menu" name="submit"/>
 			</form>
-			<form action="." method="post">
+			<form action="" method="post">
 				
 				<input type="hidden" name="action" value="pmenu_remove" /><input type="submit" value="Remove Persistent Menu" name="submit"/>
 			</form>
